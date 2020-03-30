0e4c23f9cc00928da3768dabf88310050dd80b2d
Brannua <374453156@qq.com>
2020-03-30 15:21:39 +0800

    对比原生开发方式理解插件机制，理解框架路由的实现方式

---
+++ blog-express/routes/user.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+  router = express.Router();
+
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body;
+  res.json({
+    errno: 0,
+    data: { username, password },
+  });
+});
+
+module.exports = router;
